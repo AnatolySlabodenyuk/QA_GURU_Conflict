c3211596691982542ab4c4eb71fec7b99d667b03
Anatoly <slabodenyukanatoly@gmail.com>
2023-01-03 03:34:36 +0700

    Create new test

---
+++ src/test/java/new_test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class new_test {
+    @Test
+    void successfulSearchTest() {
+        open("https://habr.com");
+    }
+}
